1f7d39b63d0fa86c6bc91352f1b1c438fcf51890
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2023-06-11 15:09:23 +0300

    Initial Commit

---
+++ CashControl/CashControlApp.swift
@@ -0,0 +1,20 @@
+//
+//  CashControlApp.swift
+//  CashControl
+//
+//  Created by Ацамаз Бицоев on 11.06.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CashControlApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
